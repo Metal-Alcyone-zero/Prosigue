27cbce0f601ca0121fb16b4fe54c4a5015051d01
Metal-Alcyone-zero <152511882+Metal-Alcyone-zero@users.noreply.github.com>
2024-08-06 17:08:33 -0500

    Actualizo a la versión 1.8
    Hay muchos cambios en esta actualización.

--- prosigue/standard/principiofinal.py
+++ prosigue/standard/principiofinal.py
@@ -8,6 +8,16 @@ import time
 
 "===================================================="
 
+class Operador:
+    
+    def empieza(self, configura, iter):
+        
+        configura[1]= "mi_carro"  # o, se puede modificar una instancia
+        iter.put(configura)
+
+        time.sleep(8)
+
+
 class Restituye:
     
     def termina(self, caso):
@@ -19,13 +29,3 @@ class Restituye:
             print("sirvase emplear una clase personal")
             print("en reemplazo de Restituye.")
             
-
-class Operador:
-    
-    def empieza(self, configura, iter):
-        
-        configura[1]= "mi_carro"  # o, se puede mod una instancia
-        iter.put(configura)
-
-        time.sleep(8)
-
